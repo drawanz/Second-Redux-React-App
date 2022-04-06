a974887e8e85d168b79fc728259abd303abf0969
Leonidas <leonidas.drawanz@gmail.com>
2022-04-06 16:51:15 -0300

    requisito 5 pela metade

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,22 @@
 export const GET_TOKEN = 'GET_TOKEN';
 export const GET_TOKEN_SUCESS = 'GET_TOKEN_SUCESS';
-export const GET_TOKEN_FAIL = 'GET_TOKEN_FAIL';
 
 export const SAVE_NAME_AND_EMAIL = 'SAVE_NAME_AND_EMAIL';
+
+// export const GET_QUESTION = 'GET_QUESTION';
+
+// export const GET_QUESTION_SUCESS = 'GET_QUESTION_SUCESS';
 
 export const actionGetToken = () => ({ type: GET_TOKEN });
 
 export const actionGetTokenSucess = (token) => ({ type: GET_TOKEN_SUCESS, token });
+
+// export const actionGetQuestion = () => ({ type: GET_QUESTION });
+
+// export const actionGetQuestionSucess = (question) => ({
+//   type: GET_QUESTION_SUCESS,
+//   question,
+// });
 
 export const thunkGetToken = () => async (dispatch) => {
   dispatch(actionGetToken());
@@ -20,3 +30,10 @@ export const saveNameAndEmail = (name, email) => ({
   name,
   email,
 });
+
+// export const thunkGetQuestion = (token) => async (dispatch) => {
+//   dispatch(actionGetQuestion());
+//   const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+//   const question = await response.json();
+//   dispatch(actionGetQuestionSucess(question.results));
+// };
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,12 +1,97 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
 
-export default class Game extends Component {
+class Game extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      questions: '',
+    };
+  }
+
+  componentDidMount() {
+    const { token } = this.props;
+    this.getQuestions(token);
+  }
+
+  getQuestions = async (token) => {
+    const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    const { results } = await response.json();
+    console.log(results);
+    this.setState({ questions: results });
+  }
+
+  // createOptions = (type) => {
+  //   if(type === 'boolean') {
+  //     const maxNumber =
+  //   }
+  // }
+
   render() {
+    const { questions } = this.state;
     return (
       <div>
         <Header />
+        <div>
+          {questions && questions.map((element) => {
+            element.type === 'boolean' ? (
+              <div>
+                <button type="button">{ element. }</button>
+              </div>
+            ) : () 
+          })}
+          {/* {questions && questions.map((element, index) => {
+            let maxNumber = 0;
+            if (element.incorrect_answers.length === 1) {
+              maxNumber = 1;
+            } else {
+              maxNumber = 2;
+            }
+            const randomNumber = Math.round(Math.random() * maxNumber);
+            const array = [];
+            for (let i = 0; i < element.incorrect_answers.length; i += 1) {
+              if (i === randomNumber || element.incorrect_answers.length === 1) {
+                array.push(element.correct_answers);
+                array.push(element.incorrect_answers[i]);
+              } else {
+                array.push(element.incorrect_answers[i]);
+              }
+            }
+            console.log(array);
+            return (
+              <div key={ `Pergunta ${index + 1}` }>
+                <h4>{ element.category }</h4>
+                <p>{ element.question }</p> */}
+                {/* {element.incorrect_answers.map((ele, i) => {
+                  if (i === randomNumber || element.incorrect_answers.length === 1) {
+                    return (
+                      <button type="button">{ element.correct_answers }</button>
+                      <button key={ ele } type="button">{ ele }</button>
+                    );
+                  }
+                  return (
+                    <button key={ ele } type="button">{ ele }</button>
+                  );
+                })} */}
+
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  token: state.token,
+});
+
+Game.propTypes = {
+  token: propTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Game);
